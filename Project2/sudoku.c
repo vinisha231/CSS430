4793e8d50f44279c4bbf89e0fbbe41e8d7b4da10
Vinisha <129325340+vinisha231@users.noreply.github.com>
2025-02-06 07:01:46 +0000

    ahhfuuuu

--- Project2/sudoku.c
+++ Project2/sudoku.c
@@ -6,17 +6,170 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// takes puzzle size and grid[][] representing sudoku puzzle
-// and tow booleans to be assigned: complete and valid.
-// row-0 and column-0 is ignored for convenience, so a 9x9 puzzle
-// has grid[1][1] as the top-left element and grid[9]9] as bottom right
-// A puzzle is complete if it can be completed with no 0s in it
-// If complete, a puzzle is valid if all rows/columns/boxes have numbers from 1
-// to psize For incomplete puzzles, we cannot say anything about validity
+typedef struct {
+  int row;
+  int column;
+  int psize;
+  int **grid;
+  bool *result;
+} parameters;
+
+#define HISTORY_SIZE 10
+#define MAXLINE 80
+#define PROMPT "osh> "
+
+char *history[HISTORY_SIZE];
+int history_count = 0;
+
+void add_to_history(char *line) {
+  if (history_count < HISTORY_SIZE) {
+    history[history_count++] = strdup(line);
+  } else {
+    free(history[0]);
+    for (int i = 1; i < HISTORY_SIZE; i++) {
+      history[i - 1] = history[i];
+    }
+    history[HISTORY_SIZE - 1] = strdup(line);
+  }
+}
+
+void print_history() {
+  for (int i = 0; i < history_count; i++) {
+    printf("%d %s\n", i + 1, history[i]);
+  }
+}
+
+void *check_row(void *param) {
+  parameters *data = (parameters *)param;
+  int row = data->row;
+  int psize = data->psize;
+  int **grid = data->grid;
+  bool *result = data->result;
+
+  bool seen[psize + 1];
+  for (int i = 1; i <= psize; i++)
+    seen[i] = false;
+
+  for (int col = 1; col <= psize; col++) {
+    int num = grid[row][col];
+    if (num < 1 || num > psize || seen[num]) {
+      *result = false;
+      pthread_exit(0);
+    }
+    seen[num] = true;
+  }
+  *result = true;
+  pthread_exit(0);
+}
+
+void *check_column(void *param) {
+  parameters *data = (parameters *)param;
+  int col = data->column;
+  int psize = data->psize;
+  int **grid = data->grid;
+  bool *result = data->result;
+
+  bool seen[psize + 1];
+  for (int i = 1; i <= psize; i++)
+    seen[i] = false;
+
+  for (int row = 1; row <= psize; row++) {
+    int num = grid[row][col];
+    if (num < 1 || num > psize || seen[num]) {
+      *result = false;
+      pthread_exit(0);
+    }
+    seen[num] = true;
+  }
+  *result = true;
+  pthread_exit(0);
+}
+
+void *check_subgrid(void *param) {
+  parameters *data = (parameters *)param;
+  int startRow = data->row;
+  int startCol = data->column;
+  int psize = data->psize;
+  int **grid = data->grid;
+  bool *result = data->result;
+
+  int subgridSize = (int)sqrt(psize);
+  bool seen[psize + 1];
+  for (int i = 1; i <= psize; i++)
+    seen[i] = false;
+
+  for (int row = startRow; row < startRow + subgridSize; row++) {
+    for (int col = startCol; col < startCol + subgridSize; col++) {
+      int num = grid[row][col];
+      if (num < 1 || num > psize || seen[num]) {
+        *result = false;
+        pthread_exit(0);
+      }
+      seen[num] = true;
+    }
+  }
+  *result = true;
+  pthread_exit(0);
+}
+
 void checkPuzzle(int psize, int **grid, bool *complete, bool *valid) {
-  // YOUR CODE GOES HERE and in HELPER FUNCTIONS
   *valid = true;
   *complete = true;
+
+  pthread_t threads[psize * 3];
+  parameters data[psize * 3];
+  bool results[psize * 3];
+
+  int threadIndex = 0;
+
+  for (int i = 1; i <= psize; i++) {
+    data[threadIndex].row = i;
+    data[threadIndex].column = 0;
+    data[threadIndex].psize = psize;
+    data[threadIndex].grid = grid;
+    data[threadIndex].result = &results[threadIndex];
+    pthread_create(&threads[threadIndex], NULL, check_row, &data[threadIndex]);
+    threadIndex++;
+
+    data[threadIndex].row = 0;
+    data[threadIndex].column = i;
+    data[threadIndex].psize = psize;
+    data[threadIndex].grid = grid;
+    data[threadIndex].result = &results[threadIndex];
+    pthread_create(&threads[threadIndex], NULL, check_column,
+                   &data[threadIndex]);
+    threadIndex++;
+  }
+
+  int subgridSize = (int)sqrt(psize);
+  for (int row = 1; row <= psize; row += subgridSize) {
+    for (int col = 1; col <= psize; col += subgridSize) {
+      data[threadIndex].row = row;
+      data[threadIndex].column = col;
+      data[threadIndex].psize = psize;
+      data[threadIndex].grid = grid;
+      data[threadIndex].result = &results[threadIndex];
+      pthread_create(&threads[threadIndex], NULL, check_subgrid,
+                     &data[threadIndex]);
+      threadIndex++;
+    }
+  }
+
+  for (int i = 0; i < threadIndex; i++) {
+    pthread_join(threads[i], NULL);
+    if (!results[i]) {
+      *valid = false;
+    }
+  }
+
+  for (int row = 1; row <= psize; row++) {
+    for (int col = 1; col <= psize; col++) {
+      if (grid[row][col] == 0) {
+        *complete = false;
+        return;
+      }
+    }
+  }
 }
 
 // takes filename and pointer to grid[][]
